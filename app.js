ca4a23e6ddd1dadf6d1fb1343c08e1eafc142c2c
Ryan Park <admin@ryanpark.co.uk>
2015-11-28 21:22:01 +0000

    Tweet and DB syncing added

--- app.js
+++ app.js
@@ -1,46 +1,179 @@
-var Xray = require('x-ray');
-var x = Xray();
+// Requirements
+var async = require('async');
+var env = require('node-env-file');
+var mongodb = require('mongodb');
+var Twitter = require('twitter')
 
-var niJobFinderGrads =    'http://www.nijobfinder.co.uk/search/262329691/Page1/';
-var niJobFinderJuniors =  'http://www.nijobfinder.co.uk/search/262330730/Page1/';
+var scrape = require('./scrape.js');
 
-var indeedGrads =   'http://www.indeed.co.uk/graduate-jobs-in-Northern-Ireland';
-var indeedJuniors = 'http://www.indeed.co.uk/junior-jobs-in-Northern-Ireland';
+// Environment vars
+env('config/config.env');
 
-var jobsArray = [];
+// Twitter setup
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+// Mongo Setup
+var mongoURL = process.env.MONGO_URL;
+var MongoClient = mongodb.MongoClient;
+
+var postTweets = function(jobs) {
+  
+  // Connect to MongoDB
+  MongoClient.connect(mongoURL, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      // Connected!
+      console.log('Connection established to', mongoURL);
+      
+      // Set collection to jobs
+      var collection = db.collection('jobs');
+      
+      // Check jobs against existing jobs
+      async.each(jobs, function(job, callback) {
+        // If we can find the job, ignore it otherwise 
+        collection.find({id: job.id}).toArray(function (err, result) {
+          if (err) {
+            // Error
+            console.log(err);
+          } else if (result.length) {
+            // Duplicate
+            console.log(job.title + " - is a duplicate, ignoring.");
+          } else {
+            // New Job, tweet about it!
+            var tweetString = job.title + " - " + job.link;
+            
+            client.post('statuses/update', {
+              status: tweetString
+            },  
+            function(error, tweet, response){
+              if(error) throw error;
+              //console.log(tweet); 
+              //console.log(response); 
+            });
+            
+            // Add to our records
+            collection.insert(job, function (err, result) {
+              if (err) {
+                console.log(err);
+               } else {
+                console.log('Added Job - ' + job.title + ' succesfully.');
+                callback();
+               }
+            });   
+          }
+        });
+      }, function(err) {
+        console.log("Finished Cycle");
+        // Close
+        db.close();
+      })
+    
+      
+    }
+  });
+  
+  
+}
+
+// Scrape configure objects
+
+var niJobFinderGrads = {
+  url : 'http://www.nijobfinder.co.uk/search/262329691/Page1/',
+  rootSelector : '.result',
+  selectors : {
+    title: '.job-title a',
+    image: '.recruiter-logo@src',
+    link: '.job-title a@href'
+  },
+  pagination: '.next a@href',
+  limit: 5,
+  customTransforms : [
+    function(url) {
+      var id = url.match(/\/\d{6}\//g)[0].replace('/', '');
+      id = id.replace('/', '');
+      
+      return {
+        property: 'id',
+        value: id
+      }
+    }
+  ]
+}
+
+var niJobFinderJuniors = {
+  url : 'http://www.nijobfinder.co.uk/search/262330730/Page1/',
+  rootSelector : '.result',
+  selectors : {
+    title: '.job-title a',
+    image: '.recruiter-logo@src',
+    link: '.job-title a@href'
+  },
+  pagination: '.next a@href',
+  limit: 5,
+  customTransforms : [
+    function(url) {
+      var id = url.match(/\/\d{6}\//g)[0].replace('/', '');
+      id = id.replace('/', '');
+      
+      return {
+        property: 'id',
+        value: id
+      }
+    }
+  ]
+}
+
+var indeedGrads = {
+  url : 'http://www.indeed.co.uk/graduate-jobs-in-Northern-Ireland',
+  rootSelector: '.result',
+  selectors: {
+    title: '.turnstileLink@title',
+    link: '.turnstileLink@href',
+    id: '@id'
+  },
+  pagination: '.pagination a:last-child@href',
+  limit: 8
+}
+
+var indeedJuniors = {
+  url : 'http://www.indeed.co.uk/junior-jobs-in-Northern-Ireland',
+  rootSelector: '.result',
+  selectors: {
+    title: '.turnstileLink@title',
+    link: '.turnstileLink@href',
+    id: '@id'
+  },
+  pagination: '.pagination a:last-child@href',
+  limit: 8
+}
 
 
-x(niJobFinderGrads, '.result', [{
-  title: '.job-title a',
-  image: '.recruiter-logo@src',
-  link: '.job-title a@href'
-}])(function(err, result){
-    jobsArray.push(result);
-  })
-  .paginate('.next a@href')
-  .paginate(niJobFinderJuniors)
-  .paginate('.next a@href')
+// Scrape!
+
+scrape.job(indeedGrads, function(jobs) {
+  postTweets(jobs);
+})
+
+scrape.job(indeedJuniors, function(jobs) {
+  postTweets(jobs);
+})
+
+scrape.job(niJobFinderGrads, function(jobs) {
+  postTweets(jobs);
+})
+
+scrape.job(niJobFinderJuniors, function(jobs) {
+  postTweets(jobs);
+})
 
 
 
-x(indeedGrads, '.result', [{
-  title: '.jobtitle@title',
-  link: '.jobtitle@href'
-}])(function(err, result){
-    jobsArray.push(result);
-  })
-  .paginate('.pagination a:last-child@href')
-  .limit(15)
-  .write('indeed.json')
 
-x(indeedGrads, '.result', [{
-  title: '.jobtitle@title',
-  link: '.jobtitle@href'
-}])(function(err, result){
-    jobsArray.push(result);
-  })
-  .paginate('.pagination a:last-child@href')
-  .limit(15)
-  .write('indeed-juniors.json')
 
 
---
+++ scrape.js
@@ -0,0 +1,72 @@
+// Requirements
+var xray = require('x-ray');
+var x = xray();
+
+// Whitelist and Blacklist
+var whitelist = [
+  "graduate", 
+  "junior", 
+  "apprentice",
+  "apprenticeship", 
+  "trainee", 
+  "developer", 
+  "tester", 
+  "technician"
+];
+
+var blacklist = [
+  "senior",
+  "experienced"
+]
+
+// Scrape Export
+exports.job = function(config, callback) {
+  
+  x(config.url, config.rootSelector, [config.selectors])(function(err, result){
+    
+    // Iterate through each scraped job
+    var filteredResults = result.filter(function(job) {
+      
+      var isValidJob = false;
+      
+      // Filter our results
+      whitelist.forEach(function(word) {
+        if (job.title.toLowerCase().indexOf(word) > -1) {
+          
+          // Flag as a valid job
+          isValidJob = true;
+          
+          // Job meets whitelist, but check for any blacklisted words too
+          blacklist.forEach(function(badword) {
+            if (job.title.toLowerCase().indexOf(badword) > -1) {
+              isValidJob = false
+            }
+          })
+          
+        } 
+      })
+
+      
+      // Approve or deny
+      if (isValidJob) {
+        // Check if we have any custom properties to derive
+        if (config.customTransforms) {
+          config.customTransforms.forEach(function(transform) {
+            var result = transform(job.link);
+            job[result.property] = result.value;
+          })
+        } 
+        return job
+      } else {
+        console.log(job.title + " is trash, ignoring.")
+      }
+      
+    });
+    
+    callback(filteredResults);
+
+  })
+  .paginate(config.pagination)
+  .limit(config.limit)
+  
+}
